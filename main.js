99381e0d41fe4dd9f115872663d111e29696aaac
unknown <rislammb@gmail.com>
2020-01-16 14:56:08 +0600

    First day

---
+++ main.js
@@ -0,0 +1,16 @@
+// // DOM Element
+// const menuBtn = document.querySelector('.menu-btn');
+// const cartLogo = document.querySelector('.cart-logo');
+// const cart = document.querySelector('.cart');
+// const cartCloseBtn = document.querySelector('.cart-close');
+
+// menuBtn.addEventListener('click', cartOpen);
+// cartLogo.addEventListener('click', cartOpen);
+// cartCloseBtn.addEventListener('click', cartClose);
+
+// function cartOpen() {
+//   cart.classList.add('open');
+// }
+// function cartClose() {
+//   cart.classList.remove('open');
+// }
