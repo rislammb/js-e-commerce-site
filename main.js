91264b395a73011198762a4e9dddb039b5b21f0c
unknown <rislammb@gmail.com>
2020-01-19 17:38:00 +0600

    After second day

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-// // DOM Element
-// const menuBtn = document.querySelector('.menu-btn');
-// const cartLogo = document.querySelector('.cart-logo');
-// const cart = document.querySelector('.cart');
-// const cartCloseBtn = document.querySelector('.cart-close');
+// Variables
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+// Cart
+let cart = [];
 
-// menuBtn.addEventListener('click', cartOpen);
-// cartLogo.addEventListener('click', cartOpen);
-// cartCloseBtn.addEventListener('click', cartClose);
-
-// function cartOpen() {
-//   cart.classList.add('open');
-// }
-// function cartClose() {
-//   cart.classList.remove('open');
-// }
+class Products {
+  getProducts() {
+    fetch('products.json');
+  }
+}
+class UI {}
+class Storage {}
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  const products = new Products();
+});
